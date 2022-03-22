7bc30c5f7fef3231fd1133e0a55dac0e892c3585
rabiaugurlu <rabiaugurluuu@gmail.com>
2022-03-22 19:29:14 +0300

    JSIntro

---
+++ JSIntro/script.js
@@ -0,0 +1,14 @@
+//alert ("hello")
+
+console.log("hello");
+console.log(123456);
+console.log(true);
+console.error("an error ocured");
+console.warn("a warning has occured");
+
+
+
+console.clear(); // clears all code written in console.
+
+// single line comment line
+/* comment lines */
