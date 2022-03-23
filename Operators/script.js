ba376ca77a8dc8a328ef3d66d3b54c566d24401e
rabiaugurlu <rabiaugurluuu@gmail.com>
2022-03-23 09:45:10 +0300

    Operators

---
+++ Operators/script.js
@@ -0,0 +1,103 @@
+/* 1)mathematical operators
+addition operator: + 
++(1,2)  prefix 
+(1+2)   infix
+(1,2)+  postfix */
+
+let a = 2;
+let b = 3;
+console.log(a + b);
+
+
+// subtraction operator: - 
+
+let c = 5;
+let d = 2;
+console.log(c - d);
+
+
+// multiplication operator: * 
+let e = 10;
+let f = 11;
+console.log(e * f);
+
+
+// division operator: / 
+
+let g = 20;
+let h = 5;
+console.log(g / h);
+
+
+// remainder: % 
+
+let k = 50;
+let l = 10;
+console.log(k % l);
+
+
+// 2)Increment/Decrement operators 
+
+let m = 20;
+console.log(++m);
+console.log(--m);
+
+
+/* 3)Comparison operators 
+> : greater-than
+< : less-than
+>= : greater than or equal to
+<= : less than or equal to
+== : equal
+!= : not equal to
+=== : type and value equal
+!== : not type and value equal */
+
+
+/* 4) Logical and Bitwise operators  
+& AND
+| OR
+^ XOR 
+~ NOT
+*/
+
+
+/* 5)Assignment operator
+= : It assigns the value on the right to the left.
+let x = 3; */
+
+/*
+6)Bitwise Shift operators
+>> : RIGHT SHIFT
+<< : LEFT SHIFT */
+
+const n = 2;
+console.log(2 << n);
+console.log(n >> 1);
+
+
+/* 7)Conditional operators
+condition ? ifTrue : ifFalse */
+
+
+/*
+8)Functional programming operators
+Arrow function : The arrow function allows defining functions that will be used to define pure function types in functional programming.
+sum =(a,b) => a+b
+Pipe operator : The pipe operator again provides a structure in the functional programming approach that makes the function chain more readable.
+*/
+
+
+// 9)Unary opetors:operand that has single operand.
+
+let x = 1;
+x = -1;
+alert(x);
+
+//10)Binary operand:operand that has two operands.
+
+let y = 3;
+let z = 2;
+alert(y - z);
+
+//operator predence : https://javascript.info/operators#operator-precedence
